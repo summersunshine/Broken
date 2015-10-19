12145d17e6173ecbe43832830d49c2fdcddd04b3
summersunshine <490179946@qq.com>
2015-10-19 09:40:10 +0800

    Init
    Init Project

---
+++ Assets/Scripts/DataStruct.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TINVoronoi
+{
+
+    public struct PointF
+    {
+        private float x,y;
+
+        //
+        // 摘要: 
+        //     用指定坐标初始化 System.Drawing.PointF 类的新实例。
+        //
+        // 参数: 
+        //   x:
+        //     该点的水平位置。
+        //
+        //   y:
+        //     该点的垂直位置。
+        public PointF(float x, float y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+
+
+        //
+        // 摘要: 
+        //     获取或设置此 System.Drawing.PointF 的 x 坐标。
+        //
+        // 返回结果: 
+        //     此 System.Drawing.PointF 的 x 坐标。
+        public float X { get { return x; } set { x = value; } }
+        //
+        // 摘要: 
+        //     获取或设置此 System.Drawing.PointF 的 y 坐标。
+        //
+        // 返回结果: 
+        //     此 System.Drawing.PointF 的 y 坐标。
+        public float Y { get { return y; } set { y = value; } }
+
+
+
+
+    }
+
+    //离散点
+    public struct Vertex
+    {
+        public long x;
+        public long y;
+        public long ID;
+        public int isHullEdge; //凸壳顶点标记,系统初始化为0
+
+        //相等则返回true
+        public static bool Compare(Vertex a, Vertex b)
+        {
+            return a.x == b.x && a.y == b.y ;
+        }
+    }
+
+    //边
+    public struct Edge
+    {
+        public long Vertex1ID;   //点索引
+        public long Vertex2ID;
+        public Boolean NotHullEdge;  //非凸壳边
+        public long AdjTriangle1ID;
+        public long AdjacentT1V3;    //△1的第三顶点在顶点数组的索引
+        public long AdjTriangle2ID;
+
+        public Edge(long iV1, long iV2)
+        {
+            Vertex1ID = iV1;
+            Vertex2ID = iV2;
+            NotHullEdge = false;
+            AdjTriangle1ID = 0;
+            AdjTriangle2ID = 0;
+            AdjacentT1V3 = 0;
+        }
+
+        //相等则返回true
+        public static bool Compare(Edge a, Edge b)
+        {
+            return ((a.Vertex1ID== b.Vertex1ID) && (a.Vertex2ID==b.Vertex2ID)) ||
+                ((a.Vertex1ID== b.Vertex2ID) && (a.Vertex2ID==b.Vertex1ID));
+        }    
+       
+    }
+
+    //三角形
+    public struct Triangle
+    {
+        public long V1Index; //点在链表中的索引值
+        public long V2Index;
+        public long V3Index;
+    }
+
+    //外接圆心
+    public struct Barycenter
+    {
+        public double X;
+        public double Y;
+    }
+
+    public struct VoronoiEdge
+    {
+        public Edge VEdge;
+    }
+
+    public struct BoundaryBox
+    {
+        public long XLeft;
+        public long YTop;
+        public long XRight;
+        public long YBottom;
+    }
+
+    public class DataStruct
+    { 
+        public static int MaxVertices=500;
+        public static int MaxEdges=2000;
+        public static int MaxTriangles = 1000;
+        public Vertex[] Vertex=new Vertex[MaxVertices];
+        public Triangle[] Triangle = new Triangle[MaxTriangles];
+        public Barycenter[] Barycenters = new Barycenter[MaxTriangles]; //外接圆心
+        public Edge[] TinEdges = new Edge[MaxEdges];
+        public BoundaryBox BBOX = new BoundaryBox();  //图副边界框
+        public int VerticesNum = 0;
+        public int TinEdgeNum = 0;
+        public int TriangleNum = 0;
+    }
+
+}
---
+++ Assets/Scripts/Window.cs
@@ -0,0 +1,209 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TINVoronoi;
+
+public class Window : MonoBehaviour {
+
+    public List<Vector3> vertexs;
+
+
+    GameObject scene;
+    GameObject plane;
+
+    Delaynay D_TIN = new Delaynay(); //核心功能类
+
+
+    void Awake()
+    {
+        plane = GameObject.Find("Plane");
+        scene = GameObject.Find("Scene");
+    }
+
+   
+	// Use this for initialization
+	void Start () {
+        Mesh mesh = plane.GetComponent<MeshFilter>().mesh;
+
+
+
+        for (int i = 0; i < 20; i++)
+        {
+            addVertex(new Vector2(UnityEngine.Random.Range(-48, 48), UnityEngine.Random.Range(-48, 48)));
+        }
+        addVertex(new Vector2(50, 50));
+        addVertex(new Vector2(50, -50));
+        addVertex(new Vector2(-50, 50));
+        addVertex(new Vector2(-49, -50));
+        ShowTriangle();
+	}
+	
+
+    public void addVertex(Vector2 e)
+    {
+        for (int i = 0; i < D_TIN.DS.VerticesNum; i++)
+        {
+            if ((long)e.x == D_TIN.DS.Vertex[i].x && (long)e.y == D_TIN.DS.Vertex[i].y)
+                return;  //若该点已有则不再加入
+        }
+
+        //加点            
+        D_TIN.DS.Vertex[D_TIN.DS.VerticesNum].x = (long)e.x;
+        D_TIN.DS.Vertex[D_TIN.DS.VerticesNum].y = (long)e.y;
+        D_TIN.DS.Vertex[D_TIN.DS.VerticesNum].ID = D_TIN.DS.VerticesNum;
+        D_TIN.DS.VerticesNum++;
+
+        //GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        //sphere.transform.parent = plane.transform;
+        //sphere.transform.localPosition = new Vector3(e.x, 0, e.y);
+        //sphere.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+        //Debug.Log(e);
+    }
+
+    private void ShowTriangle()
+    {
+        if (D_TIN.DS.VerticesNum > 2)  //构建三角网
+            D_TIN.CreateTIN();
+
+        Mesh mesh = plane.GetComponent<MeshFilter>().mesh;
+        mesh.SetVertices(getVertics(D_TIN.DS.Vertex));
+
+        int[] triangles = new int[D_TIN.DS.TriangleNum * 3];
+        for (int i = 0; i < D_TIN.DS.TriangleNum; i++)
+        {
+            triangles[3 * i + 0] = (int)D_TIN.DS.Triangle[i].V1Index;
+            triangles[3 * i + 1] = (int)D_TIN.DS.Triangle[i].V2Index;
+            triangles[3 * i + 2] = (int)D_TIN.DS.Triangle[i].V3Index;
+        }
+
+        mesh.triangles = triangles;
+        
+        plane.GetComponent<MeshCollider>().sharedMesh = mesh;
+
+        print(mesh);
+
+        splitMesh(mesh);
+
+       // plane.SetActive(false);
+       
+    }
+
+    public List<Vector3> getVertics(Vertex[] Vertex)
+    {
+        vertexs = new List<Vector3>();
+        for (int i = 0; i < Vertex.Length; i++)
+        {
+            vertexs.Add(new Vector3( Vertex[i].x,0,Vertex[i].y));
+        }
+        return vertexs;
+    }
+
+    public void printMesh(Mesh mesh)
+    {
+        for (int i = 0; i < mesh.vertices.Length; i++)
+        {
+            Debug.Log(mesh.vertices[i]);
+        }
+
+
+        for (int i = 0; i < mesh.triangles.Length; i++)
+        {
+            Debug.Log(mesh.triangles[i]);
+        }
+    }
+
+    public void splitMesh(Mesh mesh)
+    {
+       
+        for (int i = 0; i < mesh.triangles.Length;i+=3)
+        {
+            Vector3 [] vertices = new Vector3[6];
+            vertices[0] = mesh.vertices[mesh.triangles[i + 0]] + Vector3.up;
+            vertices[1] = mesh.vertices[mesh.triangles[i + 1]] + Vector3.up;
+            vertices[2] = mesh.vertices[mesh.triangles[i + 2]] + Vector3.up;
+            vertices[3] = mesh.vertices[mesh.triangles[i + 0]] - Vector3.up;
+            vertices[4] = mesh.vertices[mesh.triangles[i + 1]] - Vector3.up;
+            vertices[5] = mesh.vertices[mesh.triangles[i + 2]] - Vector3.up;
+            Debug.Log(vertices[0]);
+            Debug.Log(vertices[1]);
+            Debug.Log(vertices[2]);
+            Debug.Log(vertices[3]);
+            Debug.Log(vertices[4]);
+            Debug.Log(vertices[5]);
+
+            //Debug.Log(mesh.triangles[i]);
+            Mesh subMesh = new Mesh();
+            subMesh.vertices = vertices;
+            subMesh.triangles = getTriangles();
+            subMesh.RecalculateNormals();
+            GameObject go = GameObject.CreatePrimitive(PrimitiveType.Quad);
+          //  go.AddComponent<MeshFilter>();
+            go.GetComponent<MeshFilter>().mesh =  subMesh;
+            //go.AddComponent<MeshCollider>();
+            go.GetComponent<MeshCollider>().sharedMesh = subMesh;
+            go.GetComponent<MeshCollider>().convex = true;
+            go.AddComponent<Rigidbody>();
+            
+            go.transform.parent = scene.transform;
+
+        }
+
+    }
+
+    int[] getTriangles()
+    {
+        int[] triangles = new int[24];
+        triangles[0] = 0;
+        triangles[1] = 1;
+        triangles[2] = 2;
+
+        triangles[3] = 5;
+        triangles[4] = 4;
+        triangles[5] = 3;
+        
+        triangles[6] = 3;
+        triangles[7] = 1;
+        triangles[8] = 0;
+        
+        triangles[9] = 3;
+        triangles[10] = 4;
+        triangles[11] = 1;
+       
+        triangles[12] = 0;
+        triangles[13] = 2;
+        triangles[14] = 3;
+        
+        triangles[15] = 2;
+        triangles[16] = 5;
+        triangles[17] = 3;
+
+        triangles[18] = 1;
+        triangles[19] = 5;
+        triangles[20] = 2;
+        
+
+        triangles[21] = 1;
+        triangles[22] = 4;
+        triangles[23] = 5;
+        return triangles;
+    }
+
+    //生成并显示凸壳
+    private void ShowConvex(int interval)
+    {
+        D_TIN.CreateConvex();
+
+        for (int i = 0; i < D_TIN.HullPoint.Count; i++)
+        {
+           // g.DrawLine(Pens.Black, D_TIN.DS.Vertex[D_TIN.HullPoint[i]].x, D_TIN.DS.Vertex[D_TIN.HullPoint[i]].y,
+           //  D_TIN.DS.Vertex[D_TIN.HullPoint[(i + 1) % D_TIN.HullPoint.Count]].x,
+            // D_TIN.DS.Vertex[D_TIN.HullPoint[(i + 1) % D_TIN.HullPoint.Count]].y);
+           // Thread.Sleep(interval);
+        }
+    }
+
+
+
+
+}
